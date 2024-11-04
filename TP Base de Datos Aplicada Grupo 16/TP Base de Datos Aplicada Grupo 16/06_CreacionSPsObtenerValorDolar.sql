f80762e5cdda168ab339a08a1f7dc3da136ba93b
Lucio Rius Conde <lucioarius@gmail.com>
2024-11-04 16:04:20 -0300

    Change project sps

--- TP Base de Datos Aplicada Grupo 16/TP Base de Datos Aplicada Grupo 16/06_CreacionSPsObtenerValorDolar.sql
+++ TP Base de Datos Aplicada Grupo 16/TP Base de Datos Aplicada Grupo 16/06_CreacionSPsObtenerValorDolar.sql
@@ -1,11 +1,12 @@
 USE Com2900G16;
 GO
-CREATE OR ALTER PROCEDURE [catalogo].[ObtenerValorDolar]
-AS
+CREATE FUNCTION [catalogo].[ConversionDolarPeso] ()
+RETURNS INT
 BEGIN
 DECLARE @URL NVARCHAR(MAX) = 'https://dolarapi.com/v1/dolares/oficial';
 Declare @Object as Int;
 Declare @ResponseText as Varchar(8000);
+Declare @valor as int;
 
 Exec sp_OACreate 'MSXML2.XMLHTTP', @Object OUT;
 Exec sp_OAMethod @Object, 'open', NULL, 'get',
@@ -16,22 +17,20 @@ Exec sp_OAMethod @Object, 'responseText', @ResponseText OUTPUT
 IF((Select @ResponseText) <> '')
 BEGIN
      DECLARE @json NVARCHAR(MAX) = (Select @ResponseText)
-     SELECT venta
-     FROM OPENJSON(@json)
-          WITH (
-                 moneda VARCHAR(10) '$.moneda',
-                 casa VARCHAR(10) '$.casa',
-                 nombre VARCHAR(10) '$.nombre',
-                 compra VARCHAR(10) '$.compra',
-                 venta VARCHAR(10) '$.venta',
-                 fechaActualizacion NVARCHAR(20) '$.fechaActualizacion'
-               );
+     SET @valor = 
+		 (SELECT venta
+		 FROM OPENJSON(@json)
+			  WITH (
+					 moneda VARCHAR(10) '$.moneda',
+					 casa VARCHAR(10) '$.casa',
+					 nombre VARCHAR(10) '$.nombre',
+					 compra VARCHAR(10) '$.compra',
+					 venta VARCHAR(10) '$.venta',
+					 fechaActualizacion NVARCHAR(20) '$.fechaActualizacion'
+				   ));
 END
 ELSE
-BEGIN
-     DECLARE @ErroMsg NVARCHAR(30) = 'No data found.';
-     Print @ErroMsg;
-	 SELECT 1
-END
+	 SET @valor = 0;
 Exec sp_OADestroy @Object
+RETURN @valor
 END
